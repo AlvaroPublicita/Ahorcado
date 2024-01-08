924523b57eb9463adef3fd06df5e490ca9e19736
Alvaro Diaz <alvaropublicita@gmail.com>
2024-01-08 10:52:06 +0100

    Requerimientos iniciales del proyecto

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Crea un juego de ahorcado en el que el usuario tenga que adivinar una palabra.
+// El juego debe mostrar tantos guiones como letras tenga la palabra a adivinar.
+// El usuario podrá ir introduciendo letras que crea que pueden estar en la palabra.
+// Si la letra está en la palabra, se mostrará en lugar del guión correspondiente.
+// Si la letra no está, se irá dibujando el ahorcado.
+// El usuario tendrá 6 intentos para adivinar la palabra.
+// Si adivina la palabra antes de que se le acaben los intentos, habrá ganado.
+// Si se le acaban los intentos, habrá perdido.
+// El juego debe tener al menos 25 palabras distintas para adivinar.
+// El juego debe tener un sistema de pistas que el usuario pueda utilizar cuando quiera.
+// El juego debe tener un sistema de puntuación que guarde las partidas ganadas y perdidas.
